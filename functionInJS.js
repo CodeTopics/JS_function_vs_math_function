d24ed6d52dbf83ec74c2c9af68f2402e4574441f
CodeTopics <79413588+CodeTopics@users.noreply.github.com>
2021-03-11 21:25:15 +0530

    Create functionInJS.js

---
+++ functionInJS.js
@@ -0,0 +1,16 @@
+
+
+function addOneToSquareOfAnyNumber(input){
+
+const number = input;
+
+const output = number*number + 1;
+ 
+return output;
+}
+
+const outputValue = addOneToSquareOfAnyNumber(2);
+
+console.log("outputValue",outputValue);
+
+
